2e8fe594040b7bfe88868a325bb6a7abd25a4017
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-02-28 15:51:57 +0900

    edit画面から、情報が空の状態でprototypeを更新すると生じるエラーの解決

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,10 +25,10 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,4 +7,6 @@ class Prototype < ApplicationRecord
   validates :concept, presence: true
   validates :catch_copy, presence: true
   validates :image, presence: true
+
+  
 end
